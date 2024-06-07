a9281019d644da3b291dbc41536c3bf059ff9b00
humbl3LilaS <sabishinekobebe@gmail.com>
2024-06-07 23:42:45 +0630

    Add: helper for SqueakyCleanConverter

---
+++ src/com.exercism.exercise/SqueakyCleanConverter.java
@@ -0,0 +1,42 @@
+package com.exercism.exercise;
+
+import java.util.function.IntUnaryOperator;
+
+public class SqueakyCleanConverter implements IntUnaryOperator
+{
+	private boolean isUpperCase;
+
+
+	@Override
+	public int applyAsInt(int codePoint)
+	{
+		return switch (codePoint)
+		{
+			case '0' -> 'o';
+			case '1' -> 'l';
+			case '3' -> 'e';
+			case '4' -> 'a';
+			case '7' -> 't';
+			case '-' ->
+			{
+				isUpperCase = true;
+				yield -1;
+			}
+			default ->
+			{
+				if (!Character.isLetter(codePoint))
+				{
+					yield -1;
+				}
+				if (isUpperCase)
+				{
+					isUpperCase = false;
+					yield Character.toUpperCase(codePoint);
+				}
+				yield codePoint;
+			}
+
+
+		};
+	}
+}
